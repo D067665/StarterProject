75d069983732b8005daf9efa3827bab3b2198946
„cennet10 <cennetback@web.de>
2019-04-18 19:36:52 +0200

    Slider HistoryPage

--- StarterProject/StarterProject/HistoryPage.xaml.cs
+++ StarterProject/StarterProject/HistoryPage.xaml.cs
@@ -1,20 +1,48 @@
-﻿using System;
+﻿
+using StarterProject.Model;
+using StarterProject.ViewModel;
+using Syncfusion.ListView.XForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace StarterProject
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class HistoryPage : TabbedPage
-	{
-		public HistoryPage ()
+	public partial class HistoryPage : Xamarin.Forms.TabbedPage
+    {
+        ToolViewModel tvm;
+        public HistoryPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+            this.On<Xamarin.Forms.PlatformConfiguration.Android>().SetIsSwipePagingEnabled(false);
+            tvm = new ToolViewModel();
+
+            BindingContext = tvm;
+        }
+        private void ListView_SwipeStarted(object sender, SwipeStartedEventArgs e)
+        {
+            if (e.ItemIndex == 1)
+                e.Cancel = true;
+        }
+        private void ListView_SwipeEnded(object sender, SwipeEndedEventArgs e)
+        {
+            if (e.SwipeOffset > 70)
+                listView.ResetSwipe();
+        }
+
+        private void ListView_Swiping(object sender, SwipingEventArgs e)
+        {
+            if (e.ItemIndex == 1 && e.SwipeOffSet > 70)
+                e.Handled = true;
+        }
+    }
+
+
 }
